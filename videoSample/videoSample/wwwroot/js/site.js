127f2b378f72b23722accbb77f917801ab5a1b73
Dalton Walls <scallywig@gmail.com>
2023-03-13 21:21:04 -0400

    push

---
+++ videoSample/videoSample/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+function mouseOver(element) {
+    element.style.color = 'green';
+}
+
+function mouseOut(element) {
+    element.style.color = 'black';
+}
